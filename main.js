af992f4489d4910f2f3d5c6d41569f54dffcf0f2
Arjun Sarode <sarodearjun57@gmail.com>
2014-11-14 13:24:11 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+
+
+	var getMarginPush = function() {
+		var fullHeight = $(".center-wrapper").height()
+		var contentHeight = $(".main-wrapper").height()
+		var marginPush = (fullHeight / 2) - (contentHeight / 2)
+		return marginPush
+	}
+
+
+	var centerContent  = function() {
+		var fullHeight = $(".center-wrapper").height()
+		var contentHeight = $(".main-wrapper").height()
+		var marginPush = getMarginPush()
+		console.log(fullHeight)
+		console.log(contentHeight)
+		if (fullHeight > contentHeight) {
+			$(".main-wrapper").css("margin-top", marginPush)
+		}
+		
+	}
+
+
+	centerContent()
+	// $(".content-left-effect, .content-right-effect").height($(".fade-in").innerHeight())
+	$(".content-left-effect, .content-right-effect").css("max-height", $(".fade-in").innerHeight())
+
+	// $(".content-left-effect").height($(".main-wrapper").height())
+	// $(".content-left-effect").css("margin-top", getMarginPush() + $("h1").outerHeight(true))
+	$(".fade-in").hide()
+	$(".fade-in").fadeIn(750)
+
+
+	$(window).resize(function() {
+		centerContent()
+	})
+
+	
+
+})
