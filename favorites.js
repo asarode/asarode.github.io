060a8bb81db48fe559b6e9c8455c60e1a078f4ca
arjun <sarodearjun57@gmail.com>
2015-07-13 22:42:43 -0400

    switch from ajax call to fav list to local copy

--- favorites.js
+++ favorites.js
@@ -1,10 +1,51 @@
 $(document).ready(function() {
-	$.ajax({
-		url: "https://api.github.com/gists/79de1d997a5bd0e28ad3"
-	}).success(function(res) {
-		var favs = JSON.parse(res.files["favs.json"].content).data;
-		addFavs(favs);
-	});
+	// $.ajax({
+	// 	url: "https://api.github.com/gists/79de1d997a5bd0e28ad3"
+	// }).success(function(res) {
+	// 	var favs = JSON.parse(res.files["favs.json"].content).data;
+	// 	addFavs(favs);
+	// });
+
+	var favs = [
+		"Psycho-Pass",
+		"No Game No Life",
+		"Steins;Gate",
+		"Fullmetal Alchemist: Brotherhood",
+		"Katekyo Hitman Reborn!",
+		"Ano Hana",
+		"Commando, Infantry, or Police. Who are you?",
+		"Superhero.js",
+		"What every programmer needs to know about game networking",
+		"To understand code you have to build a mental model",
+		"Your first GraphQL Server",
+		"AngularJS Tutorial: A Comprehensive 10,000 Word Guide",
+		"Common Misconceptions About Inheritance in Javascript",
+		"Git from the inside out",
+		"Using RxJS for data flow instead of Flux with React",
+		"What is the Flux Application Architecture?",
+		"A Brief History of User Experience Design",
+		"The Sad Tragedy of Micro-Optimzation Theater",
+		"REACT",
+		"IMMUTABLE JS",
+		"RETHINK DB",
+		"MONGO DB",
+		"ALT JS",
+		"EXPRESS JS",
+		"HAPI",
+		"JOI",
+		"NODE JS",
+		"RXJS",
+		"FLUX",
+		"PASSPORT",
+		"SOCKET.IO",
+		"BLUEBIRD",
+		"ASYNC",
+		"BABEL",
+		"SWIFTLY JSON",
+		"OBOE JS"
+	];
+
+	addFavs(favs);
 
 	function addFavs(favs) {
 		var favContainer = $('<div>', {id: 'favs-background'});
